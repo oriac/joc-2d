18d4e2581a757b39ef6fa71f40de557b6235e6c0
oriac <oriac86@gmail.com>
2013-03-26 19:23:53 +0100

    ia enemy v1.0

--- cEnemy.cpp
+++ cEnemy.cpp
@@ -1,6 +1,7 @@
-
+#include <queue>
+#include <vector>
 #include "cEnemy.h"
-
+using namespace std;
 cEnemy::cEnemy() {}
 cEnemy::~cEnemy(){}
 
@@ -38,4 +39,76 @@ void cEnemy::Draw(int tex_id)
 	yf = yo - 0.25f;
 
 	DrawRect(tex_id,xo,yo,xf,yf);
+}
+void cEnemy::NextStep(int x, int y, int*map) {
+	int posx,posy;
+	int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+	int min,actual,nx,ny,mov;
+	cBicho::GetTile(&posx,&posy);
+	min = 100000;
+	mov = -1;
+	for( int k = 0; k < 4; ++k) {
+			nx = dx[k];
+			ny = dy[k];
+			actual = Logic(x,y,posx+nx,posy+ny);
+			if (actual != -1 && actual < min) {
+				min = actual;
+				mov = k;
+			}
+	}
+	switch (mov) {
+		case 0: 
+			cBicho::MoveLeft(map);
+			break;
+		case 1:
+			cBicho::MoveRight(map);
+			break;
+		case 2:
+			cBicho::MoveDown(map);
+			break;
+		case 3:
+			cBicho::MoveUp(map);
+			break;
+		case -1:
+			cBicho::Stop();
+			break;
+	}
+
+}
+int cEnemy::Logic(int x, int y,int posx, int posy)
+{
+    Estado vec1;
+	vec1.x = posx;
+	vec1.y = posy;
+	vec1.dist = 0;
+    queue <Estado > vecinos;
+    vecinos.push(vec1);
+    vector< vector<int> > visitados(25,vector<int> (25,0));
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    while( ! vecinos.empty() ){
+            Estado actual = vecinos.front();
+            vecinos.pop();
+            if(actual.x == x && actual.y == y) {
+				return actual.dist;
+            }
+            visitados[actual.x][actual.y]=1;
+            for( int k = 0; k < 4; ++k) {
+                int nx = dx[k] + actual.x;
+                int ny = dy[k] + actual.y;
+                if (nx < 25 && nx >= 0 && ny >=0 && ny < 25) {
+                    if (visitados[nx][ny] !=1) {
+                            Estado adyacente;
+                            adyacente.x = nx;
+                            adyacente.y = ny;
+							adyacente.dist = actual.dist+1;
+                            vecinos.push(adyacente);
+                            visitados[nx][ny] = 1;
+                    }
+                }
+            }
+            //vecinos.pop();
+    }
+    return -1;
 }
--- cEnemy.h
+++ cEnemy.h
@@ -5,6 +5,11 @@
 #define PLAYER_START_CX		3
 #define PLAYER_START_CY		2
 
+    typedef struct {
+            int x;
+            int y;
+			int dist;
+    } Estado;
 class cEnemy: public cBicho
 {
 public:
@@ -12,4 +17,7 @@ public:
 	~cEnemy();
 
 	void Draw(int tex_id);
+	void NextStep(int x, int y, int*map);
+	int Logic(int x, int y,int posx, int posy);
 };
+
